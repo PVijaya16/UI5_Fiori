a632ad4e46dcbdd62dd57840844b6f589754c012
Vijaya Porakapalli <vijayareddy036@gmail.com>
2026-03-27 06:49:03 +0000

    codes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Button",
+    "sap/m/Image",
+    "sap/m/VBox"
+], function (Controller, Button, Image, VBox) {
     "use strict";
 
     return Controller.extend("project4.controller.View1", {
@@ -36,25 +39,88 @@ sap.ui.define([
 
             myPromise
                 .then(resultPromise => {
-
-                    // ✅ Always get latest UI text
                     let currentText = this.getView().byId("txtResult").getText();
-
                     currentText += resultPromise;
-
                     this.getView().byId("txtResult").setText(currentText);
                 })
                 .catch(error => {
-
                     let currentText = this.getView().byId("txtResult").getText();
-
                     currentText += error;
-
                     this.getView().byId("txtResult").setText(currentText);
                 });
 
-            // ===== CALL ASYNC FUNCTION =====
+            // ===== ASYNC FUNCTION CALL =====
             this.example();
+
+            // ===== IMAGE + BUTTON LOGIC =====
+            var oRoot = this.getView().byId("rootBox");
+
+            this.oImgBox = new VBox({
+                width: "100%",
+                height: "400px"
+            });
+
+            // Open Button
+            this.oOpenBtn = new Button({
+                text: "Open Image",
+                type: "Default",
+                press: this.onShowImage.bind(this)
+            });
+
+            // Close Button
+            this.oCloseBtn = new Button({
+                text: "Close",
+                type: "Default",
+                visible: false,
+                press: this.onCloseImage.bind(this)
+            });
+
+            // Apply theme colors using controller
+            this.oOpenBtn.addEventDelegate({
+                onAfterRendering: function () {
+                    var $btn = this.$().find(".sapMBtnInner");
+                    $btn.css("background-color", "var(--sapButton_Background)");
+                    $btn.css("color", "var(--sapPositiveColor)");
+                }
+            }, this.oOpenBtn);
+
+            this.oCloseBtn.addEventDelegate({
+                onAfterRendering: function () {
+                    var $btn = this.$().find(".sapMBtnInner");
+                    $btn.css("background-color", "var(--sapButton_Background)");
+                    $btn.css("color", "var(--sapNegativeColor)");
+                }
+            }, this.oCloseBtn);
+
+            // Add controls
+            oRoot.addItem(this.oOpenBtn);
+            oRoot.addItem(this.oCloseBtn);
+            oRoot.addItem(this.oImgBox);
+        },
+
+        // ===== IMAGE FUNCTIONS =====
+        onShowImage: function () {
+
+            this.oImgBox.removeAllItems();
+
+            var oImage = new Image({
+                src: "./image/image_2.jpg",
+                width: "100%",
+                height: "100%"
+            });
+
+            this.oImgBox.addItem(oImage);
+
+            this.oOpenBtn.setVisible(false);
+            this.oCloseBtn.setVisible(true);
+        },
+
+        onCloseImage: function () {
+
+            this.oImgBox.removeAllItems();
+
+            this.oOpenBtn.setVisible(true);
+            this.oCloseBtn.setVisible(false);
         },
 
         // ===== ASYNC FUNCTION =====
@@ -62,7 +128,6 @@ sap.ui.define([
 
             let oView = this.getView();
 
-            // ✅ Get latest text
             let resultText = oView.byId("txtResult").getText();
 
             resultText += "\nAsync/Await:\nStart\n";
@@ -72,7 +137,6 @@ sap.ui.define([
                 setTimeout(() => resolve("Done!\n"), 2000);
             });
 
-            // ✅ Get latest again before updating
             resultText = oView.byId("txtResult").getText();
 
             resultText += result + "End\n";
